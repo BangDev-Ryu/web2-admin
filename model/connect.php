bc9f4de9885a160441f8c870beff5c83d6f2c618
BangDev-Ryu <ryushiki12@gmail.com>
2025-03-04 23:18:54 +0700

    fix url, update table san pham

---
+++ model/connect.php
@@ -0,0 +1,86 @@
+<?php
+class connectDB
+{
+    private $servername = "localhost";
+    private $username = "root";
+    private $databasename = "web2_sql";
+    private $password = "";
+    public $conn; // Thuộc tính chứa kết nối
+
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->databasename);
+        if ($this->conn->connect_error) {
+            die("Kết nối thất bại: " . $this->conn->connect_error);
+        }
+    }
+
+    // Thực hiện câu lệnh đã chuẩn bị
+    public function executePrepared($sql, $params)
+    {
+        $stmt = $this->conn->prepare($sql);
+        if ($stmt === false) {
+            die("Lỗi chuẩn bị truy vấn: " . $this->conn->error);
+        }
+
+        // Bind các tham số
+        if ($params) {
+            $types = '';
+            foreach ($params as $param) {
+                $types .= is_int($param) ? 'i' : 's'; // 'i' cho số nguyên, 's' cho chuỗi
+            }
+            $stmt->bind_param($types, ...$params);
+        }
+
+        // Thực hiện truy vấn
+        if ($stmt->execute()) {
+            // Nếu là câu lệnh SELECT, trả về kết quả
+            if (stripos($sql, 'SELECT') === 0) {
+                return $stmt->get_result();
+            }
+            return true; // Nếu là INSERT, UPDATE, DELETE
+        } else {
+            die("Lỗi thực thi truy vấn: " . $stmt->error);
+        }
+    }
+
+    // Đóng kết nối
+    public function close()
+    {
+        $this->conn->close();
+    }
+
+    public function query($sql) {
+        return $this->conn->query($sql);
+    }
+
+    // Lấy tất cả bản ghi từ bảng
+    public function selectAll($tableName)
+    {
+        $sql = "SELECT * FROM $tableName";
+        return $this->query($sql);
+    }
+
+    // Lấy bản ghi theo điều kiện
+    public function selectByCondition($tableName, $condition, $params)
+    {
+        $sql = "SELECT * FROM $tableName WHERE $condition";
+        return $this->executePrepared($sql, $params);
+    }
+
+    // Lấy tổng số dòng bản ghi theo điều kiện với JOIN
+    public function totalByCondition($tableName, $join = '', $condition, $params) {
+    // Sử dụng COUNT(*) để đếm tổng số dòng
+        $sql = "SELECT COUNT(*) AS total FROM $tableName $join WHERE $condition";
+        
+        $result = $this->executePrepared($sql, $params);
+        
+        if ($result instanceof mysqli_result) {
+            $row = $result->fetch_assoc();
+            return (int) $row['total'];
+        }
+        
+        return 0; // Trả về 0 nếu không có kết quả
+    }
+}
+?>
--- view/sidebar.php
+++ view/sidebar.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/sidebar.css?v=<?php echo filemtime('./css/sidebar.css'); ?>" />
     <link rel="stylesheet" href="./assets/fonts/fontawesome/css/all.min.css">
-    <script src="./js/sidebar.js"></script>
+    <script src="./js/sidebar.js?v=<?= time() ?>"></script>
 </head>
 <body>
     <div class="sidebar">
@@ -17,63 +17,63 @@
         <div class="sidebar-content">
             <ul class="item-list">
                 <li class="item">
-                    <a href="view/pages/trangChu.php" class="nav-link" data-page="trangChu">
+                    <a href="#" class="nav-link" data-page="trangChu">
                         <i class="fa-solid fa-house icon"></i>
                         <span class="link">Trang chủ</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/sanPham.php" class="nav-link" data-page="sanPham">
+                    <a href="#" class="nav-link" data-page="sanPham">
                         <i class="fa-solid fa-box icon"></i>
                         <span class="link">Sản phẩm</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/donHang.php" class="nav-link" data-page="donHang">
+                    <a href="#" class="nav-link" data-page="donHang">
                         <i class="fa-solid fa-cart-shopping icon"></i>
                         <span class="link">Đơn hàng</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/theLoai.php" class="nav-link" data-page="theLoai">
+                    <a href="#" class="nav-link" data-page="theLoai">
                         <i class="fa-solid fa-list icon"></i>
                         <span class="link">Thể loại</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/taiKhoan.php" class="nav-link" data-page="taiKhoan">
+                    <a href="#" class="nav-link" data-page="taiKhoan">
                         <i class="fa-solid fa-user icon"></i>
                         <span class="link">Tài khoản</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/quyen.php" class="nav-link" data-page="quyen">
+                    <a href="#" class="nav-link" data-page="quyen">
                         <i class="fa-solid fa-wrench icon"></i>
                         <span class="link">Quyền</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/nhapHang.php" class="nav-link" data-page="nhapHang">
+                    <a href="#" class="nav-link" data-page="nhapHang">
                         <i class="fa-solid fa-truck-fast icon"></i>
                         <span class="link">Nhập hàng</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/nhaCungCap.php" class="nav-link" data-page="nhaCungCap">
+                    <a href="#" class="nav-link" data-page="nhaCungCap">
                         <i class="fa-solid fa-building-user icon"></i>
                         <span class="link">Nhà cung cấp</span>
                     </a>
                 </li>
 
                 <li class="item">
-                    <a href="view/pages/khuyenMai.php" class="nav-link" data-page="khuyenMai">
+                    <a href="#" class="nav-link" data-page="khuyenMai">
                         <i class="fa-solid fa-tags icon"></i>
                         <span class="link">Khuyến mãi</span>
                     </a>
